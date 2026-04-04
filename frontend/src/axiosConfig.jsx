a2586304b884a8d3a5ef8037e0c77ff8cc28602e
sharathbadrinath0-dotcom <sharathbadrinath@Sharaths-MacBook-Pro.local>
2026-04-04 18:11:42 +1000

    chore: update axios baseURL to live EC2 IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: 'http://13.211.135.140:5001',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default axiosInstance;
