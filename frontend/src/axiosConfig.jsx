8a1a9af691ce4b43427a625d013c07e694aca308
sharathbadrinath0-dotcom <sharathbadrinath@Sharaths-MacBook-Pro.local>
2026-04-05 19:26:09 +1000

    fix: update all URLs to new EC2 public IP 54.252.161.237

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://13.211.135.140:5001',
+  baseURL: 'http://54.252.161.237:5001',
   headers: {
     'Content-Type': 'application/json',
   },
